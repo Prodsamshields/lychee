bb348f0808cca2102ff0dff9ea0782b0c5d078f8
Sam Shields <80023262+Prodsamshields@users.noreply.github.com>
2022-08-03 09:32:01 -0400

    Create on_remove.py

---
+++ cogs/on_remove.py
@@ -0,0 +1,26 @@
+import nextcord
+from nextcord.ext import commands
+import os
+import json
+
+
+class onguildremove(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_guild_remove(guild):
+        with open("./prefixes.json", "r") as f:
+            prefixes = json.load(f)
+        prefixes.pop(str(guild.id))
+        with open("./prefixes.json", "w") as f:
+            json.dump(prefixes, f, indent=4)
+    
+        with open("./welcome_msgs.json", "r") as f:
+            welcome_msgs = json.load(f)
+        welcome_msgs.pop[str(guild.id)]
+        with open("./welcome_msgs.json", "w") as f:
+            json.dump(welcome_msgs, f, indent=4)
+
+def setup(bot):
+    bot.add_cog(onguildremove(bot))
