469bcf2728e88a04b54d842e3543d4493fb79cc1
Sam Shields <80023262+Prodsamshields@users.noreply.github.com>
2022-08-03 10:15:56 -0400

    Update unlock_channel.py

--- cogs/unlock_channel.py
+++ cogs/unlock_channel.py
@@ -30,7 +30,7 @@ class removelockdown(commands.Cog):
                 overwrite.send_messages = True
                 await channel.set_permissions(ctx.guild.default_role, overwrite=overwrite)
                 await ctx.send(embed=embed1)
-            except:
+            except nextcord.DiscordException:
                 return
 
 
