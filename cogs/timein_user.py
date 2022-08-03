bc025c9c7bd1a3defb0f8900a6d35ded3bfbc594
Sam Shields <80023262+Prodsamshields@users.noreply.github.com>
2022-08-03 10:06:24 -0400

    Create timein_user.py

---
+++ cogs/timein_user.py
@@ -0,0 +1,29 @@
+import nextcord
+from nextcord.ext import commands
+import os
+from datetime import datetime
+import humanfriendly
+
+
+class timedin(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    @commands.has_permissions(administrator=True)
+    @commands.has_permissions(moderate_members=True)
+    async def timein(self, ctx, member: nextcord.Member=None, *, reason=None):
+        embed = nextcord.Embed(title=None, description=f'You have removed from timeout in {ctx.guild} for reason: {reason}', color=0xFF6D7A, timestamp=ctx.message.created_at)
+        
+        await member.timeout(None, reason=reason)
+        channel = await member.create_dm()
+        await channel.send(embed=embed)
+        
+        await ctx.channel.purge(limit=1)
+        embed = nextcord.Embed(description=f'{member} has been timed in for reason: {reason}', color=0xFF6D7A)
+        await ctx.channel.send(embed=embed)
+
+
+
+def setup(bot):
+    bot.add_cog(timedin(bot))
