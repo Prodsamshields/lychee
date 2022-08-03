6848d340926c0580d2d093ec63b22d0b9aff4306
Sam Shields <80023262+Prodsamshields@users.noreply.github.com>
2022-08-03 10:20:58 -0400

    Create check_welcome_message.py

---
+++ cogs/check_welcome_message.py
@@ -0,0 +1,21 @@
+import nextcord
+from nextcord.ext import commands
+import os
+import json
+
+class welcomecheck(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    @commands.has_permissions(moderate_members=True)
+    async def welcome(self, ctx):
+      with open("./welcome_msgs.json", "r") as f:
+        welcome_msg = json.load(f)
+      welcome = welcome_msg[str(ctx.guild.id)]
+      await ctx.send(f"Current welcome message for {ctx.guild} is: \"{welcome}\"")
+
+
+
+def setup(bot):
+    bot.add_cog(welcomecheck(bot))
