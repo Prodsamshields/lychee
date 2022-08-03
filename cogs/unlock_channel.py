d1daa9d2a6b937bcd045420af919b5a09802639d
Sam Shields <80023262+Prodsamshields@users.noreply.github.com>
2022-08-03 10:15:07 -0400

    Create unlock_channel.py

---
+++ cogs/unlock_channel.py
@@ -0,0 +1,39 @@
+import nextcord
+from nextcord.ext import commands
+import os
+
+
+class removelockdown(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    @commands.has_permissions(manage_channels=True)
+    async def unlock(self, ctx, channel : nextcord.TextChannel=None, reason=None):
+        emoji = '🔓'
+        
+        if channel == None:
+            channel_nums = len(ctx.guild.text_channels)
+            embed2 = nextcord.Embed(description=f'{emoji} {channel_nums}/{channel_nums} channels have been unlocked.', color=0xFF6D7A)
+            for channel in ctx.guild.text_channels:
+                channel = channel
+                overwrite = channel.overwrites_for(ctx.guild.default_role)
+                overwrite.send_messages = True
+                await channel.set_permissions(ctx.guild.default_role, overwrite=overwrite)
+            await ctx.send(embed=embed2)
+        
+        else:
+            try:
+                embed1 = nextcord.Embed(description=f'{emoji} {channel.mention} unlocked for reason: {reason}', color=0xFF6D7A) 
+                channel = channel or ctx.channel
+                overwrite = channel.overwrites_for(ctx.guild.default_role)
+                overwrite.send_messages = True
+                await channel.set_permissions(ctx.guild.default_role, overwrite=overwrite)
+                await ctx.send(embed=embed1)
+            except:
+                return
+
+
+
+def setup(bot):
+    bot.add_cog(removelockdown(bot))
